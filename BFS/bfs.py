5afea221182a2c13426c469a3c4a321756016f70
Wayne Gong <gongwayne@hotmail.com>
2019-10-28 22:17:04 +0800

    BFS implementation

---
+++ BFS/bfs.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+def search(name):
+    search_queue = deque()
+    search_queue += graph[name]
+    searched = []
+
+    while search_queue:
+        person = search_queue.popleft()
+        if person not in searched:
+            if person_is_seller(person):
+                print(person + " is a mango seller")
+            else:
+                search_queue += graph[person]
+                searched.append(person)
+    
+    return False
